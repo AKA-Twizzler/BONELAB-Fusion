e02917373fb74ffc57f1f39f051e9522bf2f66a4
AKA-Twizzler <AKA-Twizzler@users.noreply.github.com>
2026-07-05 15:29:44 -0500

    dev: Force IPv4 for mod.io thumbnail CDN downloads
    Same fix as ModioModNetworker - resolves hostnames to IPv4 only
    via SocketsHttpHandler.ConnectCallback to avoid IPv6 routing issues.

--- LabFusion/src/Downloading/ModIO/ModIOThumbnailDownloader.cs
+++ LabFusion/src/Downloading/ModIO/ModIOThumbnailDownloader.cs
@@ -1,8 +1,9 @@
-﻿using MelonLoader;
+using MelonLoader;
 
 using UnityEngine;
 
 using System.Collections;
+using System.Linq;
 
 using LabFusion.Preferences;
 
@@ -68,13 +69,26 @@ public static class ModIOThumbnailDownloader
 
     private static IEnumerator CoDownloadThumbnail(string url, Action<Texture> callback)
     {
-        var handler = new HttpClientHandler()
+        var handler = new System.Net.Http.SocketsHttpHandler
         {
-            ClientCertificateOptions = ClientCertificateOption.Automatic,
-            ServerCertificateCustomValidationCallback = (_, _, _, _) => true
+            ConnectCallback = async (context, cancellationToken) =>
+            {
+                var hostEntry = await System.Net.Dns.GetHostEntryAsync(context.DnsEndPoint.Host, cancellationToken);
+                var ipv4 = hostEntry.AddressList.First(ip => ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+                var socket = new System.Net.Sockets.Socket(ipv4.AddressFamily, System.Net.Sockets.SocketType.Stream, System.Net.Sockets.ProtocolType.Tcp)
+                {
+                    NoDelay = true
+                };
+                await socket.ConnectAsync(new System.Net.IPEndPoint(ipv4, context.DnsEndPoint.Port), cancellationToken);
+                return new System.Net.Sockets.NetworkStream(socket, ownsSocket: true);
+            },
+            ConnectTimeout = TimeSpan.FromSeconds(15)
         };
 
-        using HttpClient client = new(handler);
+        using var client = new System.Net.Http.HttpClient(handler)
+        {
+            Timeout = TimeSpan.FromSeconds(15)
+        };
 
         var responseTask = client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
 
